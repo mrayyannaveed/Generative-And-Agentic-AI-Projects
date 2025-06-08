8dbb795c1e17ac8e2d7d0f4fbe75bda1383c94b7
Rayyan Naveed <rayyannaveed33@gmail.com>
2025-06-08 06:47:18 -0700

    update code

---
+++ nd-chatbot/src/nd_chatbot/__init__.py
@@ -0,0 +1,2 @@
+def main() -> None:
+    print("Hello from nd-chatbot!")
---
+++ personal-gpt/gpt.py
@@ -0,0 +1,9 @@
+import streamlit as st
+# from helper_function import print_llm_response
+
+st.set_page_config = {
+    "page_title": "Gpt",
+    "page_icon": "🤖",
+    "layout": "wide"
+}
+
